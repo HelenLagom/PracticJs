8a1dae7ebc24bf6b07beb7269d259af23185f215
OlesiaBonnie <olesya.ferrum@gmail.com>
2023-09-16 11:21:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -80,3 +80,29 @@
 //   .concat(arr.slice(arr.indexOf("foo") + 1))
 //   .reverse()
 //   .join(" ");
+
+
+/**
+ *? Напишіть ф-цію calcTotalPrice(someStones, stonesName),
+ *? яка приймає масив об'єктів та
+ *? рядок під назвою каменю.
+ *? Функція рахує та повертає загальну вартість каменів
+ *? з таким ім'ям, ціною та кількістю з об'єкта
+ */
+
+const stones = [
+  { name: "Изумруд", price: 1300, quantity: 4 },
+  { name: "Бриллиант", price: 2700, quantity: 6 },
+  { name: "Сапфир", price: 400, quantity: 7 },
+  { name: "Щебень", price: 150, quantity: 100 },
+];
+function calcTotalPrice(someStones, stonesName) {
+    for (const item of someStones) {
+        if (item.name === stonesName)
+        {
+            return item.price*item.quantity
+            }
+   }
+}
+console.log(calcTotalPrice(stones, "Сапфир"));
+console.log(calcTotalPrice(stones, "Бриллиант"));
